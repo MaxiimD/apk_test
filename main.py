4709ec4a8124e1faae9d67dd28049a6a974616f0
MaxiimD <maximdashnov2@gmail.com>
2023-08-30 18:54:52 +0300

    test9

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.lang import Builder
+import json
 import requests
 
+KV = Builder.load_file("project.kv")
 
-class Interface(FloatLayout):
-    def display_information(self):
-        data = self.ids.textInput.text
-        self.ids.label.text = data
-        res = requests.get(
-            'https://communityconnect1234-default-rtdb.europe-west1.firebasedatabase.app/Billboard.json')
+class MyApp(App):
 
+    url = 'URL HERE' # You must add .json to the end of the URL
 
-class ProjectApp(App):
+    def patch(self, JSON):
+        to_database = json.loads(JSON)
+        requests.patch(url = self.url, json = to_database)
+
+    def post(self, JSON):
+        to_database = json.loads(JSON)
+        requests.post(url = self.url, json = to_database)
+
+    def put(self, JSON):
+        to_database = json.loads(JSON)
+        requests.put(url = self.url, json = to_database)
+
+    def delete(self, JSON):
+        requests.delete(url = self.url[:-5] + JSON + ".json")
+
+    auth_key = 'FIND YOUR AUTH KEY IN THE FIREBASE CONSEL' # Refer to the YouTube video on where to find this.
+
+    def get(self):
+        request = requests.get(self.url + '?auth=' + self.auth_key)
+        print(request.json())
+
     def build(self):
-        return Interface()
+        return KV
 
-
-ProjectApp().run()
+if __name__ == '__main__':
+    MyApp().run()
 
