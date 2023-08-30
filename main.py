c7de754be4466feb4ae9e728b613aa2cc9f202d0
MaxiimD <maximdashnov2@gmail.com>
2023-08-30 18:44:21 +0300

    test8

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+import requests
+
 
 class Interface(FloatLayout):
     def display_information(self):
-        data=self.ids.textInput.text
-        self.ids.label.text=data
+        data = self.ids.textInput.text
+        self.ids.label.text = data
+        res = requests.get(
+            'https://communityconnect1234-default-rtdb.europe-west1.firebasedatabase.app/Billboard.json')
+
+
 class ProjectApp(App):
-    pass
+    def build(self):
+        return Interface()
+
 
 ProjectApp().run()
+
